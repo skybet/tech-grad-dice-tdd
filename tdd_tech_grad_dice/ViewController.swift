72ad9cfdf29d9ddeaf29533fc9d560ac54979fce
Matthew Glover <matthew.glover@skybettingandgaming.com>
2022-09-29 21:52:15 +0100

    Added Dice images Setup initial UI with Dice UIImageViews and Roll button

--- tdd_tech_grad_dice/ViewController.swift
+++ tdd_tech_grad_dice/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceOneImageView: UIImageView!
+    @IBOutlet weak var diceTwoImageView: UIImageView!
+    @IBOutlet weak var diceThreeImageView: UIImageView!
+    @IBOutlet weak var diceFourImageView: UIImageView!
+    @IBOutlet weak var diceFiveImageView: UIImageView!
+    
+    var currentDiceRoll: [Int] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func rollDiceButtonTapped(_ sender: Any) {
+        
+        currentDiceRoll.removeAll()
+        for _ in 0...4 {
+            currentDiceRoll.append(Int.random(in: 1...6))
+        }
+        
+        diceOneImageView.image = UIImage(named: "\(currentDiceRoll[0])")
+        diceTwoImageView.image = UIImage(named: "\(currentDiceRoll[1])")
+        diceThreeImageView.image = UIImage(named: "\(currentDiceRoll[2])")
+        diceFourImageView.image = UIImage(named: "\(currentDiceRoll[3])")
+        diceFiveImageView.image = UIImage(named: "\(currentDiceRoll[4])")
+    }
 }
 
